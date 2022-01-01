63888761b280985cd72e670fdc9cd6a91d5c3994
TalhaMW <88961097+TalhaMW@users.noreply.github.com>
2022-01-01 23:15:31 +0500

    update v1.1

--- app.js
+++ app.js
@@ -3,13 +3,13 @@
 // Dom Elements
 
 let wrapper = document.querySelector('wrapper');
-let miliSec = document.querySelector('.mili');
-let doubleSec = document.querySelector('.double-sec');
+let sec = document.querySelector('.sec');
 let minute = document.querySelector('.min');
 let playBtn = document.querySelector('.play');
 let pauseBtn = document.querySelector(".pause")
 let icon = document.querySelector('i');
 let reset_Btn = document.querySelector('.reset');
+let hours = document.querySelector('.hour');
 
 // // variables
 // // functions 
@@ -24,13 +24,24 @@ let reset_Btn = document.querySelector('.reset');
 
 let counter = 0;
 let stopWatch;
+let minNum = 0;
+let hourNum=0;
 playBtn.addEventListener("click",(e)=>{
     playBtn.style="display:none"
     pauseBtn.style="display:block;"
     stopWatch = setInterval(()=>{
         counter=counter+1;
-        console.log(counter);
-    },100);
+        console.log(counter%60);
+        sec.textContent=`${counter%60}`
+        if(counter%60===0){
+            minNum=minNum+1;
+            minute.textContent=`${minNum%60}`
+        }
+        if(counter%3600===0){
+            hourNum=hourNum+1;
+            hours.textContent=hourNum;
+        }
+    },1000);
 })
 pauseBtn.addEventListener("click",(e)=>{
     pauseBtn.style="display:none"
